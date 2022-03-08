e5f5a68c4a353b365cbbcd49a97cb69c3ed028ae
Donald Bowles <donniebowles84@gmail.com>
2022-03-08 15:01:57 -0500

    Arrays

--- Assets/Scripts/QuestionSO.cs
+++ Assets/Scripts/QuestionSO.cs
@@ -7,9 +7,18 @@ public class QuestionSO : ScriptableObject
 {
     [TextArea(2,6)]
     [SerializeField] string strQuestion = "Enter new question text here";
-
+    [SerializeField] string[] strAnswers = new string[4];
+    [SerializeField] int intCorrectAnswerIndex;
     public string GetQuestion()
     {
         return strQuestion;
+    }
+    public string strGetAnswer(int index)
+    {
+        return strAnswers[index];
+    }
+    public int intGetCorrectAnswerIndex()
+    {
+        return intCorrectAnswerIndex;
     }
 }

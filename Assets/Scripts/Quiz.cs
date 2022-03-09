de36d0298e42b8fbbdba684c55c052425d99e1ac
Donald Bowles <donniebowles84@gmail.com>
2022-03-09 15:05:17 -0500

    Swapping Sprites

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -2,12 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class Quiz : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI questionText;
     [SerializeField] QuestionSO question;
     [SerializeField] GameObject[] answerButtons;
+    int intCorrectAnswerIndex;
+    [SerializeField] Sprite defaultAnswerSprite;
+    [SerializeField] Sprite correctAnswerSprite;
 
     void Start()
     {
@@ -17,6 +21,26 @@ public class Quiz : MonoBehaviour
         {
         TextMeshProUGUI buttonText = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
         buttonText.text = question.strGetAnswer(i);
+        }
+    }
+
+    public void OnAnswerSelected(int index)
+    {
+        Image buttonImage;
+
+        if(index == question.intGetCorrectAnswerIndex())
+        {
+            questionText.text = "Correct!";
+            buttonImage = answerButtons[index].GetComponent<Image>();
+            buttonImage.sprite = correctAnswerSprite;
+        }
+        else   
+        {
+           intCorrectAnswerIndex = question.intGetCorrectAnswerIndex();
+           string correctAnswer = question.strGetAnswer(intCorrectAnswerIndex);
+           questionText.text = "Sorry, the correct answer was;\n" + correctAnswer;
+           buttonImage = answerButtons[intCorrectAnswerIndex].GetComponent<Image>();
+           buttonImage.sprite = correctAnswerSprite;
         }
     }
 

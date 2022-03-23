27e4c4ff91a99b8d0e3ded9a724d41cc00b0e245
Donald Bowles <donniebowles84@gmail.com>
2022-03-23 12:56:48 -0400

    Keeping Score

---
+++ Assets/Scripts/ScoreKeeper.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreKeeper : MonoBehaviour
+{
+    
+    int intCorrectAnswers = 0;
+    int intQuestionsSeen = 0;
+
+    public int GetCorrectAnswers()
+    {
+        return intCorrectAnswers;
+    }
+
+    public void IncrementCorrectAnswers()
+    {
+        intCorrectAnswers++;
+    }
+
+    public int GetQuestionSeen()
+    {
+        return intQuestionsSeen;
+    }
+
+    public void IncrementQuestionsSeen()
+    {
+        intQuestionsSeen++;
+    }
+
+    public int CalculateScore()
+    {
+        return Mathf.RoundToInt(intCorrectAnswers / (float)intQuestionsSeen * 100);
+    }
+}
